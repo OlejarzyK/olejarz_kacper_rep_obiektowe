2043fb60ed593f6bf7c38c3f2d95820d7cfc1a64
OlejarzyK <77607422+OlejarzyK@users.noreply.github.com>
2021-03-01 09:33:02 +0100

    Add files via upload

---
+++ 01.03.2021 PRACA NA LEKCJI/TablicaDwuwymiarowa1.java
@@ -0,0 +1,21 @@
+class TablicaDwuwymiarowa{
+    public static void main(String []args){
+    }
+    public static void PrzypomnienieTablicJednowymiarowych(){
+
+    
+        int [] tab = new int[5];
+        tab[0] = 40;
+        for(int i=0; i<tab,length; i++){
+            System.out.println(tab[1]);
+        }
+
+        int [][] tab2d = new int [5][2];
+
+       for (int i=0; i<tab2d.length;i++){
+           for(int j=0; j<tab2d[i].length;j++){
+               System.out.println(tab2d[i][j])
+           }
+       }
+    }
+}
