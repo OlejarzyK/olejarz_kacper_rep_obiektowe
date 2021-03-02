109a1ff37dc6ccb08e81fddf4c235ed2d75d9154
OlejarzyK <77607422+OlejarzyK@users.noreply.github.com>
2021-03-02 10:18:31 +0100

    Create ZadanieTablica.java

--- ZadanieTablica.java
+++ ZadanieTablica.java
@@ -7,11 +7,6 @@ public class ZadanieTablic {
         tablica[0][0] = 1;
         tablica[0][1] = 2;
         tablica[0][2] = 3;
-        tablica[0][3] = 4;
-        tablica[1][0] = 1;
-        tablica[1][1] = 2;
-        tablica[1][2] = 3;
-        tablica[1][3] = 4;
         for(int i=0;i<tablica.length;i++){
         for(int j=0;j<tablica.length;j++){
         System.out.println(tablica[i][j]);
