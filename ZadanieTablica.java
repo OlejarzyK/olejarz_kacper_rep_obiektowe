c6ce588acf9ffc1284d384f18586e2a0a7730f17
OlejarzyK <77607422+OlejarzyK@users.noreply.github.com>
2021-03-02 10:15:37 +0100

    Update ZadanieTablica.java

--- ZadanieTablica.java
+++ ZadanieTablica.java
@@ -4,14 +4,14 @@ public class ZadanieTablic {
     }
     private static void Tablicadwuwymiarowa(){
         int[][] tablica = new int[2][2];
-        tab2[0][0] = 1;
-        tab2[0][1] = 2;
-        tab2[0][2] = 3;
-        tab2[0][3] = 4;
-        tab2[1][0] = 1;
-        tab2[1][1] = 2;
-        tab2[1][2] = 3;
-        tab2[1][3] = 4;
+        tablica[0][0] = 1;
+        tablica[0][1] = 2;
+        tablica[0][2] = 3;
+        tablica[0][3] = 4;
+        tablica[1][0] = 1;
+        tablica[1][1] = 2;
+        tablica[1][2] = 3;
+        tablica[1][3] = 4;
         for(int i=0;i<tablica.length;i++){
         for(int j=0;j<tablica.length;j++){
         System.out.println(tablica[i][j]);
