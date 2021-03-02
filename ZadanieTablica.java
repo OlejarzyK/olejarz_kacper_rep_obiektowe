4c36ef9c8fbe13e6469881d28ec07373a080b276
OlejarzyK <77607422+OlejarzyK@users.noreply.github.com>
2021-03-02 10:03:31 +0100

    Add files via upload

---
+++ ZadanieTablica.java
@@ -0,0 +1,21 @@
+public class ZadanieTablic {
+    public static void main(String[]args){
+        Tablicadwuwymiarowa();
+    }
+    private static void Tablicadwuwymiarowa(){
+        int[][] tablica = new int[4][3];
+        tab2[0][0] = 1;
+        tab2[0][1] = 2;
+        tab2[0][2] = 3;
+        tab2[0][3] = 4;
+        tab2[1][0] = 1;
+        tab2[1][1] = 2;
+        tab2[1][2] = 3;
+        tab2[1][3] = 4;
+        for(int i=0;i<tablica.length;i++){
+        for(int j=0;j<tablica.length;j++){
+        System.out.println(tablica[i][j]);
+ }
+        }
+    }
+}
